6d8f129e8b87f1ec9b4fb84647e0787cf37ebfdb
Alexander Zulechner <alexander.zulechner@craftworks.at>
2018-07-09 23:15:35 +0200

    chore(routing): brings routing and modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'sign-up',
+    loadChildren: './sign-up/sign-up.module#SignUpModule'
+  },
+  {
+    path: 'users',
+    loadChildren: './users/users.module#UsersModule'
+  },
+  {
+    path: '',
+    redirectTo: '',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
